ce7de6464ee591f4c4f67aa399f9ad4ec899d36b
Jon Wear <jon@dramafever.com>
2014-04-26 11:34:40 -0600

    pulling width value from command line default value is 10. Removed some other test code.

--- gridguess.go
+++ gridguess.go
@@ -4,14 +4,10 @@ import "fmt"
 import "flag"
 
 
-var width = flag.Int("width", 12, "How wide the grid is.")
+var width = flag.Int("width", 10, "How wide the grid is.")
 
 
 func main(){
-
-	// fmt.Printf("You're here.\n")
+	flag.Parse()
 	fmt.Printf("This grid width is %d.\n", *width)
-
-	num:=12
-	fmt.Printf("The number is %d.\n",num)
 }
