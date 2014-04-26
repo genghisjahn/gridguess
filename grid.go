03e43f7f700e5cad44d872a463925b853aa94e32
Jon Wear <jon@dramafever.com>
2014-04-26 12:47:21 -0600

    ran go fmt

--- grid.go
+++ grid.go
@@ -1,22 +1,20 @@
 package main
 
 import "fmt"
+
 // import "math/rand"
 
-
-type Point struct{
-	x int
-	y int
-	target bool
+type Point struct {
+	x       int
+	y       int
+	target  bool
 	explore bool
 }
 
 func (p Point) String() string {
-    return fmt.Sprintf("{X:%d, Y:%d - Target: %t, Explored %t}", p.x, p.y,p.target,p.explore)
+	return fmt.Sprintf("{X:%d, Y:%d - Target: %t, Explored %t}", p.x, p.y, p.target, p.explore)
 }
 
-type Grid struct{
+type Grid struct {
 	Points []Point
 }
-
-
--- gridguess.go
+++ gridguess.go
@@ -3,34 +3,32 @@ package main
 import "fmt"
 import "flag"
 
-
 var width = flag.Int("width", 10, "Horizontal width of the grid.")
 var height = flag.Int("height", 10, "Vertical height of the grid.")
 
-
-func main(){
+func main() {
 	flag.Parse()
 
-	point:=Point{}
-	point.x=*width
-	point.y=*height
-	fmt.Printf("Here is the point %v.\n",point)
+	point := Point{}
+	point.x = *width
+	point.y = *height
+	fmt.Printf("Here is the point %v.\n", point)
 
-	grid:=Grid{}
+	grid := Grid{}
 	grid.Points = make([]Point, 100)
 
-	v:=1
-	h:=1
-	for v<=*width{
-		for h<=*height{
-			tempPoint :=Point{}
-			tempPoint.x=h
-			tempPoint.y=v
-			h+=1
-			fmt.Printf("Here is the point %v.\n",tempPoint)
+	v := 1
+	h := 1
+	for v <= *width {
+		for h <= *height {
+			tempPoint := Point{}
+			tempPoint.x = h
+			tempPoint.y = v
+			h += 1
+			fmt.Printf("Here is the point %v.\n", tempPoint)
 		}
-		v+=1
-		h=1
+		v += 1
+		h = 1
 	}
 
 }
