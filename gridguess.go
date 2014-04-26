fabb09bc5ff8d6716c9cd9ccfd4f6218caf2cd7c
Jon Wear <jon@dramafever.com>
2014-04-26 11:37:58 -0600

    width and height are pulled from CL default value is 10 corrected some spelling

--- gridguess.go
+++ gridguess.go
@@ -4,10 +4,12 @@ import "fmt"
 import "flag"
 
 
-var width = flag.Int("width", 10, "How wide the grid is.")
+var width = flag.Int("width", 10, "Horizontal width of the grid.")
+var height = flag.Int("height", 10, "Vertical height of the grid.")
 
 
 func main(){
 	flag.Parse()
-	fmt.Printf("This grid width is %d.\n", *width)
+	fmt.Printf("This grid's width is %d.\n", *width)
+	fmt.Printf("This grid's height is %d.\n", *height)
 }
