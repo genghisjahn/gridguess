ae6245880aacaf4e049ab0f9aaf9b67c74b73eb3
Jon Wear <jon@dramafever.com>
2014-04-26 13:45:07 -0600

    removed comment

--- grid.go
+++ grid.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 import "math/rand"
 
-// import "math/rand"
 
 type Point struct {
 	x       int
@@ -22,4 +21,30 @@ type Grid struct {
 
 func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
+}
+
+func (g *Grid) Build(){
+	target_x := randInt(1, *width)
+	target_y := randInt(1, *height)
+
+	set_x := 1
+	set_y := 1
+	for set_x <= *width {
+		for set_y <= *height {
+			tempPoint := Point{}
+			tempPoint.x = set_x
+			tempPoint.y = set_y
+			set_y += 1
+
+			if tempPoint.x == target_x && tempPoint.y == target_y {
+				tempPoint.target = true
+			}
+
+			g.Points = append(g.Points, tempPoint)
+			fmt.Printf("Here is the point %v.\n", tempPoint)
+		}
+		set_x += 1
+		set_y = 1
+	}
+	fmt.Printf("Target (X,Y) (%d,%d)", target_x, target_y)
 }
--- gridguess.go
+++ gridguess.go
@@ -14,29 +14,9 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	grid := Grid{}
+	grid.Build();
 
-	target_x := randInt(1, *width)
-	target_y := randInt(1, *height)
-
-	set_x := 1
-	set_y := 1
-	for set_x <= *width {
-		for set_y <= *height {
-			tempPoint := Point{}
-			tempPoint.x = set_x
-			tempPoint.y = set_y
-			set_y += 1
-
-			if tempPoint.x == target_x && tempPoint.y == target_y {
-				tempPoint.target = true
-			}
-
-			grid.Points = append(grid.Points, tempPoint)
-			fmt.Printf("Here is the point %v.\n", tempPoint)
-		}
-		set_x += 1
-		set_y = 1
-	}
+	
 	fmt.Printf("Num of Points %v.\n", len(grid.Points))
-	fmt.Printf("Target (X,Y) (%d,%d)", target_x, target_y)
+	
 }
