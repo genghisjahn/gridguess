fd0e4ff6538d5ea832274a4519a40cedf5b69eaa
Jon Wear <jon@dramafever.com>
2014-04-29 23:13:26 -0400

    No need for all this In GuessResult Simpler object.  May not need it at all.

--- guessresult.go
+++ guessresult.go
@@ -1,46 +1,7 @@
 package main
 
-import "fmt"
-
-const (
-	cInvalid = iota
-	cNorth   = iota
-	cSouth   = iota
-	cEast    = iota
-	cWest    = iota
-	cFound   = iota
-)
-
 type GuessResult struct {
-	VerticalPosition   int
-	HorizontalPosition int
-	GuessCount         int
-}
-
-func (gr GuessResult) String() string {
-	v_part := "Invalid"
-	h_part := "Invalid"
-	if gr.VerticalPosition == cNorth {
-		v_part = "North"
-	}
-	if gr.VerticalPosition == cSouth {
-		v_part = "South"
-	}
-	if gr.VerticalPosition == cFound {
-		v_part = "Found"
-	}
-
-	if gr.HorizontalPosition == cEast {
-		h_part = "East"
-	}
-	if gr.HorizontalPosition == cWest {
-		h_part = "West"
-	}
-	if gr.HorizontalPosition == cFound {
-		h_part = "Found"
-	}
-	if gr.HorizontalPosition == cFound && gr.VerticalPosition == cFound {
-		return fmt.Sprintf("Guess #%v: You got it!", gr.GuessCount)
-	}
-	return fmt.Sprintf("Guess #%v: Target is %v and %v from your guess.", gr.GuessCount, v_part, h_part)
+	Message    int
+	Found      bool
+	GuessCount int
 }
