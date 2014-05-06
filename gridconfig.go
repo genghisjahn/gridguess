5335c315f2fa2f6b4c5e69aa175ef2241c1451c7
Jon Wear <jonwear@jons-air-2.home>
2014-05-05 22:01:40 -0400

    continue work on .json config import

--- gridconfig.go
+++ gridconfig.go
@@ -1,11 +1,17 @@
+package main
+
 import (
-    "encoding/json"
-    "os"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 func GetConfig(path string) {
 	file, _ := os.Open("config.json")
 	decoder := json.NewDecoder(file)
-	dimensions = make([]Dimension, 5, 5)
+	dimensions := make([]Dimension, 5, 5)
+	err := decoder.Decode(&dimensions)
+	if err != nil {
+		fmt.Printf("No Errors on Config!\n")
+	}
 }
